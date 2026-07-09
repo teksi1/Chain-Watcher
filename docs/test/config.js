ca2ae88685448d307eacd609d05c4f0f357495b8
teksi1 <83545982+teksi1@users.noreply.github.com>
2026-07-09 16:55:41 +0300

    Add configuration for Chain Watcher API

---
+++ docs/test/config.js
@@ -0,0 +1,5 @@
+window.CHAIN_WATCHER_API_CONFIG = Object.freeze({
+  // Deployed Apps Script Web app /exec URL.
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxtoKs_eQhvd9uiYP41psiMiTgPUoN61pStGlHv1GSaj3NB5akgUrkUxsrvNJC1dwsJUA/exec',
+  timeoutMs: 30000,
+});
