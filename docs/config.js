315f42478ec4dfb867c34792c53dc851c0cdd656
teksi1 <83545982+teksi1@users.noreply.github.com>
2026-07-09 17:19:40 +0300

    Rename CHAIN_WATCHER_CONFIG to CHAIN_WATCHER_API_CONFIG

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,5 @@
-window.CHAIN_WATCHER_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwfX3vgrqPHVrzangE3W8W_u2u5Ckyx2r52Q_gwT2Mq26QreK_EZitYB0SjsJa8OXwP/exec'
+window.CHAIN_WATCHER_API_CONFIG = Object.freeze({
+  // Deployed Apps Script Web app /exec URL.
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwfX3vgrqPHVrzangE3W8W_u2u5Ckyx2r52Q_gwT2Mq26QreK_EZitYB0SjsJa8OXwP/exec',
+  timeoutMs: 30000,
 });
